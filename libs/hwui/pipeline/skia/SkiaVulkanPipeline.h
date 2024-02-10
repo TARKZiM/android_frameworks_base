44341f029ef09a244c573dde022788d5d1ad18d5
TARKZiM <tom8476oo@gmail.com>
2024-02-11 03:44:19 +0800

    Revert "hwui: port "hwui: reset to android-13.0.0_r13" to 14"
    This reverts commit 6d868f8baaabc74077e42e16132666c948219a31.

--- libs/hwui/pipeline/skia/SkiaVulkanPipeline.h
+++ libs/hwui/pipeline/skia/SkiaVulkanPipeline.h
@@ -66,6 +66,7 @@ protected:
 private:
     renderthread::VulkanManager& vulkanManager();
     renderthread::VulkanSurface* mVkSurface = nullptr;
+    sp<ANativeWindow> mNativeWindow;
 };
 
 } /* namespace skiapipeline */
